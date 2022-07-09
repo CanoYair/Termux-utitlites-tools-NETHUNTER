8b2fcb865788f0cdcde9b4e7cac165cacdd5b154
robot95 <104598388+Robot9554tt@users.noreply.github.com>
2022-07-08 21:03:40 -0500

    Add files via upload

---
+++ termux-h.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+
+pkg update && pkg upgrade -y
+pkg update
+pkg upgrade
+
+pkg update
+pkg upgrade
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install git
+pkg install coreutils
+pkg install wget nano tmux zsh neofetch curl
+
+pkg install ruby perl php clang 
+pkg install bsdtar wget proot tergent tmux openssh
+pkg install neofetch
+pkg install vim php perl
+pkg install cmus htop tmux ranger
+pkg install nano neovim python python2
+pkg install cmatrix
+
+  sudo apt-get -f install &&
+    sudo apt-get autoremove &&
+    sudo apt-get -y autoclean &&
+    sudo apt-get -y clean
+
+
+pkg install python
+pkg install python2
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip2 install requests
+
+termux-setup-storage
+
+apt install bash-completion
+apt-get install openssh
+
+apt install unstable-repo -y
+apt update
+apt install ruby python clang -y
+pkg install ruby perl php clang 
+pkg install bsdtar wget proot tergent tmux openssh
+pkg install neofetch
+pkg install vim php perl
+pkg install cmus htop tmux ranger
+pkg install nano neovim python python2
+
+
+pkg update && pkg upgrade -y
+pkg update
+pkg upgrade
+
+
+apt-get update
+
+apt-get upgrade
+
+termux-setup-storage
+
+pkg install wget
+
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+
+chmod +x install-nethunter-termux
+
+./install-nethunter-termux
+
+nh -r 
+
+git clone https://github.com/yadavvivek654/tweakandtools
+
+ls
+cd tweakandtool
+ls
+chmod +x tweakandtools-kex-repair
+
+passwd-kex
+
+start-kex
+
+stop-kex
+
+start-kex
+
+
